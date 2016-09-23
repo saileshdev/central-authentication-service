51a26a4b6d1a3ac21d6baf73575a5d03b8a6c522
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-23 20:13:09 +0530

    Add constructor for signup class

--- api/lib/api/services/signup.rb
+++ api/lib/api/services/signup.rb
@@ -1,4 +1,10 @@
 module Api::Services
   class Signup
+    
+    def initialize email: "me@example.com", password: "blank_password"
+      @email = email
+      @password = password
+    end 
+  
   end
 end
