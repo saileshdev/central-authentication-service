cf4fb744ea095bf877a56669c4f96756923aba1d
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-23 20:21:04 +0530

    Define readers for emial,user,status

--- api/lib/api/services/signup.rb
+++ api/lib/api/services/signup.rb
@@ -1,6 +1,6 @@
 module Api::Services
   class Signup
-    
+    attr_reader :email, :user, :status
     def initialize email: "me@example.com", password: "blank_password"
       @email = email
       @password = password
