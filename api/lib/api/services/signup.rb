001226f3b0f34d2786d47e9f84181ed4fd3edff0
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-23 20:29:18 +0530

    Instantiate the user. Find the encrypted password. Save the user.

--- api/lib/api/services/signup.rb
+++ api/lib/api/services/signup.rb
@@ -1,12 +1,15 @@
 module Api::Services
   class Signup
-    attr_reader :email, :user, :status
+    attr_reader :email, :user, :status, :encrypted_password
     def initialize email: "me@example.com", password: "blank_password"
       @email = email
       @password = password
     end 
   
     def call
+      @user = User.new email: @email
+      @user.encrypted_password = Digest::SHA1.hexdigest @password
+      @user.save
     end
 
   end
