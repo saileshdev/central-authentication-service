f3cbf7d2cfef894309a667cc87e53fe4098c7e74
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 00:08:20 +0530

    Add test to generate login ticket for fresh user. Also test for expiry.

---
+++ api/test/services/login_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+
+module Api::Services
+  
+  describe Login do
+    let(:user) { spawn_user }
+
+    it "generates a login ticket for a fresh login attempt" do
+      service = Login.new
+      service.call
+
+      service.ticket.must_be_kind_of LoginTicket
+    end
+    
+    describe "given an existing login ticket" do
+      attr_reader :login_ticket
+
+      before do
+        @login_ticket = spawn_login_ticket
+      end
+
+      it "expires a login ticket after an unsuccessful auth attempt" do
+        lt = login_ticket.name
+
+        service = Login.new(
+          username: "bad_user",
+          password: "bad_password",
+          login_ticket_name: lt,
+          service: "https://app.example.com"
+        )
+
+        service.call
+        login_ticket.reload.wont_be :active?
+      end
+    end
+  
+  end
+
+end
+    
