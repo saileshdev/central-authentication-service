61693212349a58d369e890716276a76b674f6fcb
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 00:17:32 +0530

    Create a login ticket and save it when call method is invoked

--- api/lib/api/services/login.rb
+++ api/lib/api/services/login.rb
@@ -1,7 +1,10 @@
 module Api::Services
   class Login
+    attr_reader :ticket
     
     def call
+      @ticket = LoginTicket.new name: "LT-#{Digest::SHA1.hexdigest(placeholder)}"
+      @ticket.save
     end
   
   end
