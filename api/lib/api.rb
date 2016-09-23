1dfefaaa467522d807d4a993288af79ef3b4a1af
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 00:22:31 +0530

    Require login_ticket in the main context

--- api/lib/api.rb
+++ api/lib/api.rb
@@ -2,6 +2,7 @@ require "api/version"
 require "api/services/signup"
 require "api/services/login"
 require "api/models/user"
+require "api/models/login_ticket"
 
 module Api
   # Your code goes here...
