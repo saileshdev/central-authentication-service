f4a6c99280d82e96a2fdc8262bae0ef4b53c8f22
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 00:34:13 +0530

    Define constructor for the LoginTicket class

--- api/lib/api/services/login.rb
+++ api/lib/api/services/login.rb
@@ -2,6 +2,14 @@ module Api::Services
   class Login
     attr_reader :ticket
     
+    def initialize username: nil, password: nil, login_ticket_name: nil, service: nil, ticket_granting_ticket_name: nil
+      @username = username
+      @password = password
+      @login_ticket_name = login_ticket_name
+      @service = service
+      @ticket_granting_ticket_name = ticket_granting_ticket_name
+    end
+    
     def call
       @ticket = LoginTicket.new name: "LT-#{Digest::SHA1.hexdigest(Time.new.to_s)}"
       @ticket.save
