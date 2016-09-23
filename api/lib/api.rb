2b8cea4efe485085a78e27aba38ecdff0d462962
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 01:23:44 +0530

    Add sevice ticket in the main context

--- api/lib/api.rb
+++ api/lib/api.rb
@@ -4,6 +4,7 @@ require "api/services/login"
 require "api/models/user"
 require "api/models/login_ticket"
 require "api/models/ticket_granting_ticket"
+require "api/models/service_ticket"
 
 module Api
   # Your code goes here...
