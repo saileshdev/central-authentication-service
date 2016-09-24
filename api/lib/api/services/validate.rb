7ecd8ec85a5ef4ae8fa003c0f671370655ef7b9f
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 13:35:03 +0530

    Add arguments yo the constructor

--- api/lib/api/services/validate.rb
+++ api/lib/api/services/validate.rb
@@ -1,7 +1,9 @@
 module Api::Services
   class Validate
   
-    def initialize
+    def initialize service, service_ticket_name
+      @service = service
+      @service_ticket_name = service_ticket_name
     end
 
     def call
