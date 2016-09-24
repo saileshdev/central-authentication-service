6f1eb56b00fc248e4f4e582cef1ba263a4333504
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 12:41:10 +0530

    Include validate.rb in the main context

--- api/lib/api.rb
+++ api/lib/api.rb
@@ -1,6 +1,7 @@
 require "api/version"
 require "api/services/signup"
 require "api/services/login"
+require "api/services/validate"
 require "api/models/user"
 require "api/models/login_ticket"
 require "api/models/ticket_granting_ticket"
