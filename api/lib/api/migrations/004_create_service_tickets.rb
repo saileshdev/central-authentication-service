7e4c6906f40e2d245642aa74fa34347e705a6318
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 12:49:12 +0530

    Add a service column in the service_tickets table

--- api/lib/api/migrations/004_create_service_tickets.rb
+++ api/lib/api/migrations/004_create_service_tickets.rb
@@ -2,6 +2,7 @@ class CreateServiceTickets < ActiveRecord::Migration
   def change
     create_table :service_tickets do |t|
       t.string :name, null: false
+      t.string :service
       t.timestamps
     end
   end
