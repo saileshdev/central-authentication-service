72e8f8236263012e59be8b9e5410276ca978948c
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 12:35:34 +0530

    Create a before hook to invoke call method on Validate class

--- api/test/services/validate_test.rb
+++ api/test/services/validate_test.rb
@@ -7,7 +7,11 @@ module Api::Services
       let(:user) { spawn_user }
       let(:service_ticket) { spawn_service_ticket service: service, user: user }
       let(:validate_service) { Validate.new(service, service_ticket.name) }
-
+      
+      before do
+      validate_service.call
+      end  
+      
     end
 end
 
