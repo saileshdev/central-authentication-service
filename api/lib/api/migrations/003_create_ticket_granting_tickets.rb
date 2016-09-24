7ed1474de64b5fd86bda19fe54681df86c842190
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 12:05:10 +0530

    Add references to user table in ticket_granting_tickets table

--- api/lib/api/migrations/003_create_ticket_granting_tickets.rb
+++ api/lib/api/migrations/003_create_ticket_granting_tickets.rb
@@ -2,6 +2,7 @@ class CreateTicketGrantingTickets < ActiveRecord::Migration
   def change
     create_table :ticket_granting_tickets do |t|
       t.string :name, null: false
+      t.references :user
       t.timestamps
     end
   end
