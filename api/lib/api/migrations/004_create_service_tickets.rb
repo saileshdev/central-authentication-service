77b061ff27e1d645df2cd793ac24f5383bae1bd0
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 12:50:35 +0530

    Add references to user table in service_tickets table

--- api/lib/api/migrations/004_create_service_tickets.rb
+++ api/lib/api/migrations/004_create_service_tickets.rb
@@ -3,6 +3,7 @@ class CreateServiceTickets < ActiveRecord::Migration
     create_table :service_tickets do |t|
       t.string :name, null: false
       t.string :service
+      t.references :user
       t.timestamps
     end
   end
