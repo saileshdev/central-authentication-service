445b5f26ad6ec78d73eaf3c6cd968819e54580d1
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 16:55:51 +0530

    Define variables for the test examples

--- api/test/api_validate_test.rb
+++ api/test/api_validate_test.rb
@@ -4,4 +4,8 @@ require "api/app/app"
 describe App do
   include Rack::Test::Methods
 
+  let(:app) { App }
+  let(:user) { spawn_user email: "me@example.com", password: "password" }
+  let(:service) { "https://app.example.com" }
+
 end
