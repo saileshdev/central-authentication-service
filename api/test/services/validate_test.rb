1873d56bf1d1fd52a7b190c8bd8eec1a209acdbd
D.Sailesh Dev <saileshdev@gmail.com>
2016-09-24 12:34:50 +0530

    Set the vars required for the test examples

--- api/test/services/validate_test.rb
+++ api/test/services/validate_test.rb
@@ -2,6 +2,12 @@ require "test_helper"
 
 module Api::Services
     describe Validate do
+
+      let(:service) { "https://app.example.com" }
+      let(:user) { spawn_user }
+      let(:service_ticket) { spawn_service_ticket service: service, user: user }
+      let(:validate_service) { Validate.new(service, service_ticket.name) }
+
     end
 end
 
